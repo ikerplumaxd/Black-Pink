bf5415e77c0d0bdc733ce8927e0d48d6184438eb
ikerplumaxd <62967256+ikerplumaxd@users.noreply.github.com>
2020-03-31 19:57:48 -0600

    Add files via upload

---
+++ Datos.php
@@ -0,0 +1,23 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$db="formulario";
+$con=mysqli_connect($host,$user,$pass,$db);
+    $NomCompleto=$_POST["NomCompleto"];
+    $Año=$_POST["Año"];
+    $Mes=$_POST["Mes"];
+    $Día=$_POST["Día"];
+    $Correo=$_POST["Correo"];
+    $HM=$_POST["HM"];
+    $Telefono=$_POST["Telefono"];
+    $Contraseña=$_POST["Contraseña"];
+
+$insertar="INSERT INTO datos (NomCompleto,Año,Mes,Día,Correo,HM,Telefono,Contraseña)  VALUES('$NomCompleto','$Año','$Mes','$Día','$Correo','$HM','$Telefono','$Contraseña')";
+$query=mysqli_query($con,$insertar);
+if(!$query){
+echo "hubo un error";}
+else {
+  echo "todo bien :D";
+}
+?>
